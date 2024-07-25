c23e2b38cfc6282200c77a50f9ef900ae330baa8
Ariel Leon Flores <arielleon24@gmail.com>
2024-07-25 16:01:30 -0400

    fixed grid and button height

--- src/components/heroMain/index.js
+++ src/components/heroMain/index.js
@@ -10,49 +10,48 @@ const [isPlaying, setIsPlaying] = useState(false)
   return (
     <main className="flex flex-col justify-center px-4 py-20 mx-auto w-full bg-appBackground leading-[140%] max-w-[480px] md:max-w-[1440px]">
       <BadgeGroup />
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-6 relative z-10">
         <h1 className="flex mt-10 text-5xl mx-auto font-semibold text-center leading-[53px] text-neutralBlack md:max-w-[804px] md:leading-[]">
           The ultimate influencer discovery platform.
         </h1>
         <p className="mt-6 text-lg text-center text-neutral-800">
           So good you'll keep it forever.
         </p>
-        <button className='px-6 py-3 text-base text-white rounded border bg-neutralBlack hover:bg-slate-700 transition-colors duration-30 self-center'>
+        <button className='z-10 px-6 py-3 text-base text-white rounded border bg-neutralBlack hover:bg-slate-700 transition-colors duration-30 self-center'>
         Book a demo
         </button>
       </div>
-      {/* PUT BACKGROUND HERE */}
-      <div className="flex overflow-hidden relative">
-        <img/>
-        <div className="mt-20 w-full h-full relative min-h-[260.56px] ">
-      {!isPlaying ? (
-        <>
-          <div className="relative mx-auto w-full h-full ">
-            <img 
-              src={placeholderImg} 
-              alt="Placeholder" 
-              className="mx-auto rounded-xl md:max-w-[1058px] md:h-[565px] md:top-[1px] md:left-[192px] md:border md:rounded-[26px] md:min-h-[565px]" 
-            />
-            <button 
-              onClick={() => setIsPlaying(true)} 
-              className="absolute inset-0 flex items-center justify-center opacity-80 hover:opacity-100 transition-opacity duration-300"
-            >
-              <svg width="137" height="137" viewBox="0 0 137 137" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="0.5" y="0.78125" width="136" height="136" rx="68" fill="#97BEF9" fill-opacity="0.3"/>
-              <rect x="26.5" y="26.7812" width="84" height="84" rx="42" fill="#E0E0E0"/>
-              <path d="M58.5031 50.7822V86.7822L88.5031 68.7822L58.5031 50.7822Z" fill="black"/>
-              </svg>
-            </button>
-          </div>
-        </>
-      ) : (
-        <iframe
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1"
-          title="YouTube video player"
-          allow="autoplay; fullscreen; picture-in-picture"
-          className="w-full h-auto mx-auto rounded-xl min-h-[250px] md:max-w-[1058px] md:h-[565px] md:top-[1px] md:left-[192px] md:border md:rounded-[26px] md:min-h-[565px]"
-        ></iframe>
-      )}
+      {/* TOFIX GRID GOES ABOVE BUTTON ON PAGE */}
+      <div className="relative z-0 md:bg-grid-gray-200 md:-mt-6 md:h-[764px]">
+        <div className="relative mt-20 w-full md:h-[565px] mx-auto">
+          {!isPlaying ? (
+            <>
+              <div className="relative flex items-center justify-center w-full h-full">
+                <img 
+                  src={placeholderImg} 
+                  alt="Placeholder" 
+                  className="mx-auto rounded-xl md:max-w-[1058px] md:h-[565px] md:top-[1px] md:left-[192px] md:border md:rounded-[26px] md:min-h-[565px]" 
+                />
+                <button 
+                  onClick={() => setIsPlaying(true)} 
+                  className="absolute inset-0 flex items-center justify-center opacity-80 hover:opacity-100 transition-opacity duration-300"
+                >
+                  <svg width="137" height="137" viewBox="0 0 137 137" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <rect x="0.5" y="0.78125" width="136" height="136" rx="68" fill="#97BEF9" fill-opacity="0.3"/>
+                  <rect x="26.5" y="26.7812" width="84" height="84" rx="42" fill="#E0E0E0"/>
+                  <path d="M58.5031 50.7822V86.7822L88.5031 68.7822L58.5031 50.7822Z" fill="black"/>
+                  </svg>
+                </button>
+              </div>
+            </>
+          ) : (
+            <iframe
+              src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1"
+              title="YouTube video player"
+              allow="autoplay; fullscreen; picture-in-picture"
+              className="w-full h-auto mx-auto rounded-xl min-h-[250px] md:max-w-[1058px] md:h-[565px] md:top-[1px] md:left-[192px] md:border md:rounded-[26px] md:min-h-[565px]"
+            ></iframe>
+          )}
         </div>
       </div>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-
 const defaultTheme = require('tailwindcss/defaultTheme')
+
+const svgToDataUri = require("mini-svg-data-uri");
+ 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette"); 
+
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -12,6 +20,7 @@ module.exports = {
         neutralGrey: '#212121',
         appBackground: '#F5F5F4',
         badgeGroup: '#EEEEEE',
+        gridColor: '#F5F5F5',
       },
       animation: {
         shimmer: "shimmer 8s infinite",
@@ -32,6 +41,18 @@ module.exports = {
     },
   },
   variants: [],
-  plugins: [],
+  plugins: [ 
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },],
 }
 
