6b9fe237152a665f36ba2506febea39c06601fc2
Ariel Leon Flores <arielleon24@gmail.com>
2024-07-30 11:07:20 -0400

    fixed video load delay

--- src/components/heroMain/index.js
+++ src/components/heroMain/index.js
@@ -6,6 +6,7 @@ const placeholderImg = 'https://s3-alpha-sig.figma.com/img/9720/5dcb/8a74186efae
 
 function HeroMain() {
 const [isPlaying, setIsPlaying] = useState(false)
+const [isLoaded, setIsLoaded] =useState(false)
 
   return (
     <main className="flex flex-col justify-center px-4 py-20 mx-auto w-full bg-appBackground leading-[140%] max-w-[480px] md:max-w-[1440px]">
@@ -21,37 +22,56 @@ const [isPlaying, setIsPlaying] = useState(false)
         Book a demo
         </button>
       </div>
-      {/* TOFIX GRID GOES ABOVE BUTTON ON PAGE */}
+      {/* TOFIX GRID should fit entire width */}
       <div className="relative z-0 md:bg-grid-gray-200 md:-mt-6 md:h-[764px]">
-        <div className="relative mt-20 w-full md:h-[565px] mx-auto">
-          {!isPlaying ? (
-            <>
-              <div className="relative flex items-center justify-center w-full h-full">
-                <img 
-                  src={placeholderImg} 
-                  alt="Placeholder" 
-                  className="mx-auto rounded-xl md:max-w-[1058px] md:h-[565px] md:top-[1px] md:left-[192px] md:border md:rounded-[26px] md:min-h-[565px]" 
-                />
-                <button 
-                  onClick={() => setIsPlaying(true)} 
-                  className="absolute inset-0 flex items-center justify-center opacity-80 hover:opacity-100 transition-opacity duration-300"
-                >
-                  <svg width="137" height="137" viewBox="0 0 137 137" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <rect x="0.5" y="0.78125" width="136" height="136" rx="68" fill="#97BEF9" fill-opacity="0.3"/>
-                  <rect x="26.5" y="26.7812" width="84" height="84" rx="42" fill="#E0E0E0"/>
-                  <path d="M58.5031 50.7822V86.7822L88.5031 68.7822L58.5031 50.7822Z" fill="black"/>
-                  </svg>
-                </button>
+        <div className="relative mt-20 w-full md:h-[565px]">
+        {!isPlaying ? (
+        <>
+          <div className="relative flex items-center justify-center w-full h-full">
+            <img 
+              src={placeholderImg} 
+              alt="Placeholder" 
+              className="mx-auto rounded-xl md:max-w-[1058px] md:h-[565px] md:top-[1px] md:left-[192px] md:border md:rounded-[26px] md:min-h-[565px]" 
+            />
+            <button 
+              onClick={() => setIsPlaying(true)} 
+              className="absolute inset-0 flex items-center justify-center opacity-80 hover:opacity-100 transition-opacity duration-300"
+            >
+              <svg width="137" height="137" viewBox="0 0 137 137" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect x="0.5" y="0.78125" width="136" height="136" rx="68" fill="#97BEF9" fill-opacity="0.3"/>
+              <rect x="26.5" y="26.7812" width="84" height="84" rx="42" fill="#E0E0E0"/>
+              <path d="M58.5031 50.7822V86.7822L88.5031 68.7822L58.5031 50.7822Z" fill="black"/>
+              </svg>
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          {!isLoaded && (
+            <div className="relative flex items-center justify-center w-full h-full">
+              <img 
+                src={placeholderImg} 
+                alt="Placeholder" 
+                className="mx-auto rounded-xl md:max-w-[1058px] md:h-[565px] md:top-[1px] md:left-[192px] md:border md:rounded-[26px] md:min-h-[565px]" 
+              />
+              <div className="absolute inset-0 flex items-center justify-center opacity-80 hover:opacity-100 transition-opacity duration-300">
+                <svg width="137" height="137" viewBox="0 0 137 137" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="0.5" y="0.78125" width="136" height="136" rx="68" fill="#97BEF9" fill-opacity="0.3"/>
+                <rect x="26.5" y="26.7812" width="84" height="84" rx="42" fill="#E0E0E0"/>
+                <path d="M58.5031 50.7822V86.7822L88.5031 68.7822L58.5031 50.7822Z" fill="black"/>
+                </svg>
               </div>
-            </>
-          ) : (
-            <iframe
-              src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1"
-              title="YouTube video player"
-              allow="autoplay; fullscreen; picture-in-picture"
-              className="w-full h-auto mx-auto rounded-xl min-h-[250px] md:max-w-[1058px] md:h-[565px] md:top-[1px] md:left-[192px] md:border md:rounded-[26px] md:min-h-[565px]"
-            ></iframe>
+            </div>
           )}
+          <iframe
+            src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1"
+            title="YouTube video player"
+            allow="autoplay; fullscreen; picture-in-picture"
+            onLoad={() => setIsLoaded(true)}
+            className={`w-full h-auto mx-auto rounded-xl min-h-[250px] md:max-w-[1058px] md:h-[565px] md:top-[1px] md:left-[192px] md:border md:rounded-[26px] md:min-h-[565px] ${isLoaded ? '' : 'hidden'}`}
+          ></iframe>
+        </>
+      )}
         </div>
       </div>
     </main>
